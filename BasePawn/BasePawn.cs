93eae43de15bcb2b317ebcd059eed87ad55b645a
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-04 14:27:12 -0400

    Add initial BaseEntity and BasePawn

---
+++ BasePawn/BasePawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePawn : MonoBehaviour, IBaseEntity {
+  int t;
+
+  void Start() { t = 0; }
+
+  void Update() {
+    Debug.Log("time = " + t);
+    t = t + 1;
+  }
+}

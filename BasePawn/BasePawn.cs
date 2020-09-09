77a7a029208e645172a386ae7a921091a12fea75
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-09 14:01:02 -0400

    Add camera component to BasePawn

--- BasePawn/BasePawn.cs
+++ BasePawn/BasePawn.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasePawn : MonoBehaviour, IBaseEntity {
+  /* TODO remove test */
   int t;
 
-  void Start() { t = 0; }
+  void Start() {
+    /* camera component */
+    Camera camera = gameObject.AddComponent<Camera>();
+
+    /* TODO remove test */
+    t = 0;
+  }
 
   void Update() {
+    /* TODO remove test */
     Debug.Log("time = " + t);
     t = t + 1;
   }

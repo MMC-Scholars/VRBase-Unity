33392d52f7364a379943c7436c13e092be9d1131
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-14 13:58:44 -0400

    Create BaseController

---
+++ BaseController/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A BaseController is a BaseEntity representing a VR motion controller.
+ * Two controllers are possessed by BasePawn to represent hands.
+ */
+
+public class BaseController : MonoBehaviour, IBaseEntity {
+    public int t;
+
+    /**
+     * Unity method
+     * Called on game start
+     */
+
+    void Start() { t = 0; }
+
+    /**
+     * Unity method
+     * Called on each update frame
+     */
+
+    void Update() {
+        Debug.Log("t = " + t);
+        t = t + 1;
+    }
+}

21fea7ea535bdc49b513bf91a71d5a0918e63398
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-14 13:57:05 -0400

    Format (indent) files

--- System/Globals.cs
+++ System/Globals.cs
@@ -8,9 +8,9 @@
  */
 
 public class Globals {
-  // since members of globals will use the namespace name Globals, we use camel
-  // case immediate member names, e.g. usage will be
-  // Globals.myProp
-  // or
-  // Globals.myClass.method()
+    // since members of globals will use the namespace name Globals, we use camel
+    // case immediate member names, e.g. usage will be
+    // Globals.myProp
+    // or
+    // Globals.myClass.method()
 }
